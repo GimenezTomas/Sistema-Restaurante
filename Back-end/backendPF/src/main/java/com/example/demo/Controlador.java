06db35e106edd682351aff26e6fb438132bbe8a9
GimenezTomas <63616281+GimenezTomas@users.noreply.github.com>
2020-11-04 23:57:24 -0300

    backend

--- Back-end/backendPF/src/main/java/com/example/demo/Controlador.java
+++ Back-end/backendPF/src/main/java/com/example/demo/Controlador.java
@@ -1,38 +1,22 @@
 package com.example.demo;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.mongodb.client.result.UpdateResult;
-import com.mongodb.util.JSONParseException;
-import org.bson.Document;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.bind.annotation.*;
 
-import java.io.File;
 import java.io.IOException;
-import java.util.*;
+import java.util.HashMap;
 
 @Controller
 public class Controlador {
 
     @Autowired
-    private AccesoMongoDB accesoMongoDB;
-
-    @Autowired
-    private Servicio servicioParaSubirArchivos;
-
-    private File json;
+    private ManejarJson accesoMongoDB;
 
     public Controlador() {
-        this.json = new File(".src/main/resources/files/archivoJSON");
-        this.accesoMongoDB = new AccesoMongoDB();
-        this.servicioParaSubirArchivos = new Servicio();
+        this.accesoMongoDB = new ManejarJson();
     }
 
     @GetMapping("/")
@@ -40,247 +24,29 @@ public class Controlador {
         return "rest";
     }
 
-    @PostMapping("procesar")
-    public ResponseEntity<Object> uploadFile(@RequestParam("file") MultipartFile file) {
+    @RequestMapping(value = "/", method = RequestMethod.POST)
+    public ResponseEntity<Object> enviarData(@RequestParam int idMesa, @RequestParam int idRestaurante){
+        HashMap<String, Object> dataUser = accesoMongoDB.dataUserAlaAPI(idRestaurante);
+        HashMap<String, Object> seccionesPlatosAlaAPI = accesoMongoDB.seccionesPlatosAlaAPI(idRestaurante);
+        HashMap<String, Object> platosYaPedidos = accesoMongoDB.platosYApedidosAlaAPI(idMesa, idRestaurante);
+        HashMap<String, Object> tiposBox = accesoMongoDB.tiposPlatosAlaAPI();
 
+        HashMap<String, Object> paquete = new HashMap<>();
+        paquete.put("dataUser", dataUser);
+        paquete.put("seccionesPlatos", seccionesPlatosAlaAPI);
+        paquete.put("platosYaPedidos", platosYaPedidos);
+        paquete.put("tiposBox", tiposBox);
+        return new ResponseEntity<>(paquete, HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/", method = RequestMethod.POST)
+    public ResponseEntity<Object> obtenerPedidoDeLaApi(@RequestBody HashMap platosPedido, @RequestParam int idMesa, @RequestParam int idRestaurante){
         try {
-            procesarArchivo(file);
+            this.accesoMongoDB.insertarPlatoPedido(idMesa, idRestaurante, platosPedido);
         } catch (IOException e) {
             e.printStackTrace();
         }
-
-        Map<String,Object> datos = this.obtenerInformacionSolicitada();
-        return new ResponseEntity<>(datos, HttpStatus.OK);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
-
-    public void procesarArchivo(MultipartFile archivo) throws IOException {
-        servicioParaSubirArchivos.guardarArchivo(archivo);
-        this.guardarDatosALaBase();
-    }
-
-    public void obtenerPlatos(int nPedido){
-        Pedido pedido =  accesoMongoDB.obtenerPedido(nPedido);
-
-        if (pedido.getnPedido()>0) {
-            ArrayList<HashMap<String, Object>> platoJSONformat = accesoMongoDB.platosPedidoMONGO(pedido.getPlatos());
-            File jsonPedido = new File(".src/main/resources/files/jsonPedido");
-
-            try{
-                ObjectMapper mapper = new ObjectMapper();
-
-                mapper.writeValue(jsonPedido, platoJSONformat);
-            }catch (JsonProcessingException e){
-                e.printStackTrace();
-            }catch (IOException e){
-                e.printStackTrace();
-            }
-        }else{
-            System.out.println("No existe ese pedido");
-        }
-
-
-    }
-
-    public List<Pedido> obtenerListaDePedidos() throws IOException {
-
-        List<Pedido> pedidoList= new ArrayList<>();
-
-        ObjectMapper mapper1 = new ObjectMapper();
-        HashMap pedidosMAP = mapper1.readValue(json, HashMap.class);
-        System.out.println(pedidosMAP);
-        return pedidoList;
-        //que tiene el json que desglozo? solo los pedidos o todo? varia?
-        //siempre es el mismo y va cambiando?
-    }
-
-    public void guardarPedidos() throws IOException {
-        List<Pedido> pedidos = obtenerListaDePedidos();
-
-    }
-
-    public HashMap<String,Long> obtenerEquipoConCantidadDeSocios(){
-
-        HashMap<String,Long> equiposConCantidadDeSocios = new HashMap<>();
-        HashSet<String> equipos = this.accesoABaseDeDatos.obtenerEquipos();
-
-        for (String nombreDeEquipo : equipos) {
-            long cantidadDeSocios = this.obtenerCantidadDeSocios(nombreDeEquipo);
-            equiposConCantidadDeSocios.put(nombreDeEquipo,cantidadDeSocios);
-        }
-        System.out.println(equiposConCantidadDeSocios);
-        return equiposConCantidadDeSocios;
-    }
-
-    public List<String> obtenerEquiposDeMayorAMenorSegunSocios(){
-
-        HashMap<String,Long> equiposConSocios = this.obtenerEquipoConCantidadDeSocios();
-
-        List<Long> cantidadesDeSocios = new ArrayList<>(equiposConSocios.values());
-        Collections.sort(cantidadesDeSocios);
-
-        List<String> nombresDeEquipos = new ArrayList<>();
-
-        for (int i = cantidadesDeSocios.size() - 1; i >= 0; i--) {
-            for (Map.Entry<String,Long> elemento : equiposConSocios.entrySet()){
-                if (elemento.getValue() == cantidadesDeSocios.get(i)){
-                    nombresDeEquipos.add(elemento.getKey());
-                }
-            }
-        }
-
-        System.out.println("equiposOrdenados" + nombresDeEquipos);
-        return nombresDeEquipos;
-    }
-
-    public long obtenerCantidadTotalDeRegistros(){
-        long cantidadTotal = accesoABaseDeDatos.obtenerCantidadDeDocumentos();
-        return cantidadTotal;
-    }
-
-    public int obtenerCantidadDeSocios(String nombreDeEquipo){
-        List<Socio> sociosDeEquipo = this.obtenerSociosDeEquipo(nombreDeEquipo);
-        int cantidadDeSocios = sociosDeEquipo.size();
-        return cantidadDeSocios;
-    }
-
-    public List<Socio> obtenerSociosDeEquipo(String nombreDeEquipo){
-
-        HashMap<String,String> filtro = new HashMap<>();
-        filtro.put("equipo",nombreDeEquipo);
-        List<Socio> sociosDeEquipo = accesoABaseDeDatos.obtenerSociosCon(filtro);
-
-        return sociosDeEquipo;
-    }
-
-    public int obtenerMenorEdadRegistradaPorEquipo(String nombreDeEquipo){
-        List<Socio> socios = this.obtenerSociosDeEquipo(nombreDeEquipo);
-        socios = Lista.ordenarMenorAMayorSegunEdad(socios);
-        int menorEdadRegistrada = socios.get(0).getEdad();
-        return menorEdadRegistrada;
-    }
-
-    public int obtenerMayorEdadRegistradaPorEquipo(String nombreDeEquipo){
-        List<Socio> socios = this.obtenerSociosDeEquipo(nombreDeEquipo);
-        socios = Lista.ordenarMayorAMenorSegunEdad(socios);
-        int menorEdadRegistrada = socios.get(0).getEdad();
-        return menorEdadRegistrada;
-    }
-
-    public float obtenerEdadPromedioDeSocios(String nombreDeEquipo){
-
-        List<Socio> sociosDeEquipo = this.obtenerSociosDeEquipo(nombreDeEquipo);
-        float edadPromedio = this.obtenerEdadPromedio(sociosDeEquipo);
-
-        return edadPromedio;
-    }
-
-    public float obtenerEdadPromedio(List<Socio> socios){
-
-        long cantidadDeSocios = socios.size();
-        long sumaTotalDeEdades = 0;
-
-        for (Socio socio : socios) {
-            sumaTotalDeEdades += socio.getEdad();
-        }
-
-        float edadPromedio = sumaTotalDeEdades / cantidadDeSocios;
-        return edadPromedio;
-    }
-
-
-
-    public List<HashMap<String,Object>> obtenerCienPrimerosSociosCasadosUniversitarios(){
-
-        HashMap<String,String> requisitos = new HashMap<>();
-        requisitos.put("estadoCivil","Casado");
-        requisitos.put("nivelDeEstudios","Universitario");
-
-        List<Socio> sociosBuscados = accesoABaseDeDatos.obtenerSociosCon(requisitos);
-        sociosBuscados = Lista.ordenarMayorAMenorSegunEdad(sociosBuscados);
-
-        List<Socio> cienPrimeros = new ArrayList<>();
-
-        if (sociosBuscados.size() >=100 ) {
-            cienPrimeros = sociosBuscados.subList(0, 100);
-        } else {
-            cienPrimeros = sociosBuscados;
-        }
-
-        List<HashMap<String,Object>> datos = new ArrayList<>();
-
-        for (Socio socio : cienPrimeros) {
-            HashMap<String,Object> datosDeSocio = new HashMap<>();
-            datosDeSocio.put("nombre",socio.getNombre());
-            datosDeSocio.put("edad",socio.getEdad());
-            datosDeSocio.put("equipo",socio.getEquipo());
-            datos.add(datosDeSocio);
-        }
-
-        return datos;
-    }
-
-    public List<String> obtenerCincoNombresMasComunes(String nombreDeEquipo){
-
-        HashMap<String,String> requisitos = new HashMap<>();
-        requisitos.put("equipo",nombreDeEquipo);
-
-        List<Socio> sociosBuscados = accesoABaseDeDatos.obtenerSociosCon(requisitos);
-        List<String> nombresDeSociosBuscados = Lista.obtenerNombresDeSocios(sociosBuscados);
-
-        SortedMap aparicionesSegunNombre = new TreeMap(java.util.Collections.reverseOrder());
-
-        for (String nombre : nombresDeSociosBuscados) {
-            int apariciones = Collections.frequency(nombresDeSociosBuscados,nombre);
-            aparicionesSegunNombre.put(nombre,apariciones);
-        }
-
-        nombresDeSociosBuscados = new ArrayList<>(aparicionesSegunNombre.keySet());
-        nombresDeSociosBuscados = nombresDeSociosBuscados.subList(0,5);
-
-        return nombresDeSociosBuscados;
-    }
-
-    public List<HashMap<String,Object>> obtenerInfoGeneralDeEquipos(){
-
-        List<HashMap<String,Object>> equipos = new ArrayList<>();
-        List<String> equiposRegistrados = this.obtenerEquiposDeMayorAMenorSegunSocios();
-
-        for (String nombreEquipo : equiposRegistrados) {
-
-            HashMap<String,Object> infoEquipo = new HashMap<>();
-            infoEquipo.put("nombre",nombreEquipo);
-
-            int cantidadDeSocios = this.obtenerCantidadDeSocios(nombreEquipo);
-            infoEquipo.put("cantidadDeSocios",cantidadDeSocios);
-
-            float edadPromedio = this.obtenerEdadPromedioDeSocios(nombreEquipo);
-            infoEquipo.put("edadPromedio",edadPromedio);
-
-            int menorEdadRegistrada = this.obtenerMenorEdadRegistradaPorEquipo(nombreEquipo);
-            infoEquipo.put("menorEdadRegistrada",menorEdadRegistrada);
-
-            int mayorEdadRegistrada = this.obtenerMayorEdadRegistradaPorEquipo(nombreEquipo);
-            infoEquipo.put("mayorEdadRegistrada",mayorEdadRegistrada);
-
-            equipos.add(infoEquipo);
-
-        }
-
-        return equipos;
-    }
-
-    public Map<String,Object> obtenerInformacionSolicitada(){
-
-        Map<String,Object> informacion = new HashMap<>();
-
-        informacion.put("cantidadTotalDePersonasRegistradas",this.obtenerCantidadTotalDeRegistros());
-        informacion.put("edadPromedioDeSociosDeRacing",this.obtenerEdadPromedioDeSocios("Racing"));
-        informacion.put("cienPrimerasPersonas",this.obtenerCienPrimerosSociosCasadosUniversitarios());
-        informacion.put("cincoNombresMasComunesDeRiver",this.obtenerCincoNombresMasComunes("River"));
-        informacion.put("listaDeEquiposSegunCantidadDeSocios",this.obtenerInfoGeneralDeEquipos());
-
-        return informacion;
-    }
-
 }
 
--- Back-end/backendPF/src/main/java/com/example/demo/ManejarJson.java
+++ Back-end/backendPF/src/main/java/com/example/demo/ManejarJson.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.mongodb.client.model.Filters;
 import org.bson.Document;
 import org.bson.conversions.Bson;
+import org.springframework.stereotype.Service;
 
 import java.io.File;
 import java.io.IOException;
@@ -12,17 +13,18 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
 
+@Service
 public class ManejarJson {
     private AccesoMongoDB mongo;
 
-    public void insertarPlatoPedido(int mesa, int idRest) throws IOException {
+    public void insertarPlatoPedido(int mesa, int idRest, HashMap platoPedido) throws IOException {
 //        File json = new File(".\\src\\main\\resources\\files\\jsonPedidos.json");
         File json = new File(".\\src\\main\\resources\\files\\collectionRestaurante.json");
         Bson filtro1= Filters.eq("id", 1);
         Pedido pedidoMONGO = mongo.obtenerPedido(1, 2, filtro1);//deberia de traer solo los abiertos
 
         ObjectMapper mapper1 = new ObjectMapper();
-        HashMap platoPedido = mapper1.readValue(json, HashMap.class);
+        //HashMap platoPedido = mapper1.readValue(json, HashMap.class);
 
         Document doc = new Document(platoPedido);
         ArrayList<PlatoPedido> platoPedidos = mongo.serializarPlatoPedido(doc);
@@ -34,45 +36,48 @@ public class ManejarJson {
         }
     }
 
-    public void seccionesPlatosAlaAPI(){
-        ObjectMapper mapper = new ObjectMapper();
-        Bson filtro1= Filters.eq("id", 1);
+    public HashMap<String, Object> seccionesPlatosAlaAPI(int id){
+        //ObjectMapper mapper = new ObjectMapper();
+        Bson filtro1= Filters.eq("id", id);
 
         HashMap<String, Object> jsonASER = new HashMap<>();
         jsonASER.put("seccionesPlatos", mongo.obtenerSecciones(filtro1));
-        try {
+        return jsonASER;
+        /*try {
             mapper.writeValue(new File(".\\src\\main\\resources\\files\\seccionesPlatos.json"), jsonASER);
         } catch (IOException e) {
             e.printStackTrace();
-        }
+        }*/
     }
 
-    public void tiposPlatosAlaAPI(){
+    public HashMap<String, Object> tiposPlatosAlaAPI(){
         ObjectMapper mapper = new ObjectMapper();
         HashMap<String, Object> jsonASER = new HashMap<>();
 
         jsonASER.put("tiposDeComida", mongo.obtenerTiposPlatos());
-        try {
+        return jsonASER;
+        /*try {
             mapper.writeValue(new File(".\\src\\main\\resources\\files\\tiposDeComida.json"), jsonASER);
         } catch (IOException e) {
             e.printStackTrace();
-        }
+        }*/
     }
 
-    public void dataUserAlaAPI(){
+    public HashMap<String, Object> dataUserAlaAPI(int id){
         ObjectMapper mapper = new ObjectMapper();
         HashMap<String, Object> jsonASER = new HashMap<>();
-        Bson filtro1= Filters.eq("id", 1);
+        Bson filtro1= Filters.eq("id", id);
 
-        jsonASER.put("tiposDeComida", mongo.obtenerDataUser(filtro1));
-        try {
+        jsonASER.put("dataUser", mongo.obtenerDataUser(filtro1));
+        return jsonASER;
+        /*try {
             mapper.writeValue(new File(".\\src\\main\\resources\\files\\dataUser.json"), jsonASER);
         } catch (IOException e) {
             e.printStackTrace();
-        }
+        }*/
     }
 
-    public void platosYApedidosAlaAPI(int mesa, int idRest){
+    public HashMap<String, Object> platosYApedidosAlaAPI(int mesa, int idRest){
         ObjectMapper mapper = new ObjectMapper();
         HashMap<String, Object> jsonASER = new HashMap<>();
         Bson filtro1= Filters.eq("id", 1);
@@ -83,19 +88,12 @@ public class ManejarJson {
             jsonASER.put("platosYaPedidos", new ArrayList<>());
             e.printStackTrace();
         }
-        try {
+        return jsonASER;
+        /*try {
             mapper.writeValue(new File(".\\src\\main\\resources\\files\\pedidosYaPedidos.json"), jsonASER);
         } catch (IOException e) {
             e.printStackTrace();
-        }
-    }
-    public static void main(String [] args) throws IOException {
-        ManejarJson m = new ManejarJson();
-        //m.insertarPlatoPedido();
-        m.dataUserAlaAPI();
-        m.seccionesPlatosAlaAPI();
-        m.platosYApedidosAlaAPI(1, 1);
-        m.tiposPlatosAlaAPI();
+        }*/
     }
 }
 // db.restaurante.aggregate([{$match:{id:2}},{ $project: { pedidos: { $filter: { input: "$pedidos", as: "pedido", cond: { $eq: [ "$$pedido.abierto", true ] } } }, _id:0 } } ]) -> devuelve los pedidos del id : 1 que esten abiertos
