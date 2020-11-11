e1f820aa5ab4956d1bdcbdc3b9483e01f314a6a6
GimenezTomas <63616281+GimenezTomas@users.noreply.github.com>
2020-11-11 11:25:56 -0300

    z

--- APP/src/com/company/Peticion.java
+++ APP/src/com/company/Peticion.java
@@ -1,12 +1,32 @@
 package com.company;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.HttpEntity;
+
+import java.io.IOException;
 import java.util.HashSet;
 
 public interface Peticion {
     static HashSet<Mesa> obtenerMesa(){
         HashSet<Mesa> mesas = new HashSet<>();
 
-        //CloseableHttpResponse response = null;
+        CloseableHttpResponse response = null;
+
+        try{
+            CloseableHttpClient httpClient = HttpClients.createDefault();
+            HttpGet request = new HttpGet("http://localhost:8080/api/javaAPP/obtenerMesas/1");
+            response = httpClient.execute(request);
+            HttpEntity entity
+        } catch (ClientProtocolException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
         return mesas;
     }
