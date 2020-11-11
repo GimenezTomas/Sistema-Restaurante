0a8e7bfb03e32c9af4bcc98822cc65f516e3fc9e
GimenezTomas <63616281+GimenezTomas@users.noreply.github.com>
2020-11-11 11:34:07 -0300

    a

--- APP/src/com/company/Peticion.java
+++ APP/src/com/company/Peticion.java
@@ -7,6 +7,7 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.HttpEntity;
+import org.apache.http.util.EntityUtils;
 
 import java.io.IOException;
 import java.util.HashSet;
@@ -21,7 +22,10 @@ public interface Peticion {
             CloseableHttpClient httpClient = HttpClients.createDefault();
             HttpGet request = new HttpGet("http://localhost:8080/api/javaAPP/obtenerMesas/1");
             response = httpClient.execute(request);
-            HttpEntity entity
+            HttpEntity entity = response.getEntity();
+            String resultado = EntityUtils.toString(entity);
+            System.out.println(resultado);
+            response.close();
         } catch (ClientProtocolException e) {
             e.printStackTrace();
         } catch (IOException e) {
